3843ca713f2b79f6d6f5cbfc75e72634c99bf64b
Ravi Gairola <mallox@pyxzl.net>
2023-11-13 20:10:55 -0800

    Use updated plugin interface & package name scoped now

--- driver.openai.js
+++ driver.openai.js
@@ -10,40 +10,75 @@ import OpenAI from 'openai-api'
  * @implements {Driver}
  */
 export default class OpenAIDriver {
-    #config;
-    #openai;
+  #thread
+  #config
+  #openai
+  static #assistants = {}
 
-    /**
-     * Creates a new OpenAI driver.
-     * @param {OpenAIConfig} config
-     */
-    constructor(config) {
-        this.#config = config
-        this.#openai = new OpenAI(config.apiKey)
-    }
+  /**
+   * Creates a new OpenAI driver.
+   * @param {API} api The API object that allows to interact with the system.
+   * @param {string} name The name of the agent to create the driver for.
+   * @param {OpenAIConfig} config The configuration object for this driver.
+   */
+  constructor(api, config, name) {
+    this.#config = config
+    this.#openai = new OpenAI(config.apiKey)
+    this.#initialize(name).catch(api.log.error)
+    api.log.info('Created OpenAI driver for agent', name)
+    api.log.trace('OpenAI driver config:', config)
+  }
 
-    /**
-     * Returns the type of the driver which is 'openai'.
-     * @override
-     * @return {string}
-     */
-    get type() {
-        return 'openai'
-    }
+  /**
+   * Returns the type of the driver which is 'openai'.
+   * @override
+   * @return {string}
+   */
+  get type() {
+    return 'openai'
+  }
 
-    async instruct(prompt) {
-        return this.#openai.complete({
-            engine: this.#config.engine,
-            prompt: prompt,
-            maxTokens: this.#config.maxTokens,
-            temperature: this.#config.temperature,
-            topP: this.#config.topP,
-            presencePenalty: this.#config.presencePenalty,
-            frequencyPenalty: this.#config.frequencyPenalty,
-            bestOf: this.#config.bestOf,
-            n: this.#config.n,
-            stream: false,
-            stop: ['\n', "Human:", "AI:"]
-        })
+  /**
+   * Returns the configuration object for this driver.
+   * @return {OpenAIConfig}
+   */
+  get config() {
+    return this.#config
+  }
+
+  /**
+   * Creates the assistant specified by the configuration, or established connection with an existing agent.
+   */
+  async #initialize(name) {
+    // Communication between agents can be done in individual threads, a single shared thread, or any combination of the two. this could allow configuration of complex interactions between agents. For now we just isolate everyone for simplicity sake.
+
+    // if individual thread:
+    // Create a new thread for this agent
+    // if global thread:
+    // if thread doesn't exist create it
+    // if group thread:
+    // for each group we're memeber of:
+    // {
+    // get assistant with name and add to this thread
+    // }
+  }
+
+  /**
+   * Gets the assistant with the given name. If it doesn't exist, it will be created.
+   * @param {string} name The name of the assistant to get.
+   * @param {OpenAIConfig} config The configuration object for this assistant if it needs to be created, otherwise it will be ignored.
+   * @return {Promise<OpenAI.Assistant>}
+   */
+  async #getAssistant(name, config) {
+    if (!this.#assistants[name]) {
+      // TODO: create assistant with name and config
+      OpenAIDriver.#assistants[name] = {to:'do'};
     }
+    return OpenAIDriver.#assistants[name];
+  }
+
+  async instruct(prompt) {
+    // TODO: send prompt to this.#thread and return response
+    // TODO: instead of a response maybe we should have a streaming response interface. Gotta see when we play with the API.
+  }
 }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import OpenAIDriver from './driver.openai.js'
 
 export function initialize(api) {
-    api.log.info('Initializing OpenAI plugin.')
     api.registerAgentDriver('openai', OpenAIDriver)
 }
