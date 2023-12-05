cd7d2a1c5fe809697f06bf77b346ec5ec86797b1
Ravi Gairola <mallox@pyxzl.net>
2023-12-04 16:57:05 -0800

    Clean up & first implementation running with OpenAI

--- driver.openai.js
+++ driver.openai.js
@@ -1,37 +1,71 @@
+import OpenAI from 'openai'
+import Run from './run.js'
+
 /**
  * @typedef {import('./api').DriverConfig} OpenAIConfig
  * @property {string} apiKey The API key to use for the OpenAI API.
+ * @property {string} model The model to use for the OpenAI API.
+ * @property {boolean} keepAssistant Whether to keep the assistant alive after the process exits. By default, its deleted.
+ * @property {boolean} keepThread Whether to keep the thread alive after the process exits. By default, its deleted.
  */
-
-import OpenAI from 'openai-api'
 
 /**
  * This is the driver for the OpenAI API.
  * @implements {Driver}
  */
 export default class OpenAIDriver {
-  static #assistants = {}
-  static #interval
-  #thread
   #config
-  #openai
   #api
+  #openai
   #agentName
+  #assistant
+  #thread
+  #messageQueue = []
+  #run
+  #available = false
 
   /**
    * Creates a new OpenAI driver.
    * @param {API} api The API object that allows to interact with the system.
    * @param {string} name The name of the agent for which this driver is running.
-   * @param {OpenAIConfig} config The configuration object for this driver.
+   * @param {AgentConfig} config The configuration object for this driver.
+   * @param {OpenAIConfig} config.driver The configuration object specific to OpenAI.
+   * @param {string} instructions The initial set of instructions to use for the assistant.
    */
-  constructor(api, name, config) {
+  constructor({api, name, config, instructions}) {
     this.#api = api
     this.#agentName = name
     this.#config = config
-    this.#openai = new OpenAI(config.apiKey)
-    this.#initialize()
-    api.log.info('Created OpenAI driver for agent', name)
-    api.log.trace('OpenAI driver config:', config)
+    this.#openai = new OpenAI({apiKey: config.driver.apiKey})
+    this.#asyncConstructor(api, name, config, instructions).catch(api.log.error)
+    api.log.debug('Created OpenAI driver for agent', name)
+    api.log.debug('OpenAI driver config:', config.driver) //trace
+  }
+
+  /**
+   * Runs the asynchronous code of the constructor
+   * @param {API} api
+   * @param {string} name
+   * @param {AgentConfig} config
+   * @param {OpenAIConfig} config.driver
+   * @param {string} instructions
+   */
+  async #asyncConstructor(api, name, config, instructions) {
+    const assistant = await this.#openai.beta.assistants.create({
+      name,
+      instructions,
+      tools: config.skills,
+      model: config.driver.model
+    })
+    this.#assistant = assistant
+    process.on('exit', () => this.#cleanup(assistant))
+
+    this.#thread = await this.#openai.beta.threads.create()
+    this.#available = true
+
+    if (this.#messageQueue.length) {
+      process.nextTick(() => this.instruct(this.#messageQueue.shift())) // For some reason the driver thinks it's not available when it is, so we need to wait a tick before instructing
+    }
   }
 
   /**
@@ -48,54 +82,103 @@ export default class OpenAIDriver {
    * @return {OpenAIConfig}
    */
   get config() {
-    return this.#config
-  }
-
-  /**
-   * Creates the assistant specified by the configuration, or established connection with an existing agent.
-   * Note that the api doesn't know about the agent yet when the driver is created so trying to get it here will fail.
-   */
-  #initialize() {
-    // Communication between agents can be done in individual threads, a single shared thread, or any combination of the two. this could allow configuration of complex interactions between agents. For now, we just isolate everyone for simplicity’s sake.
-    // if individual thread:
-    // Create a new thread for this agent
-    // if global thread:
-    // if thread doesn't exist add it
-    // if group thread:
-    // for each group we're member of:
-    // {
-    // get assistant with name and add to this thread
-    // }
-  }
-
-  /**
-   * Gets the assistant with the given name. If it doesn't exist, it will be created.
-   * @param {string} name The name of the assistant to get.
-   * @param {OpenAIConfig} config The configuration object for this assistant if it needs to be created, otherwise it will be ignored.
-   * @return {Promise<Object>} OpenAI assistant object.
-   */
-  // eslint-disable-next-line no-unused-vars
-  async #getAssistant(name, config) {
-    if (!this.#assistants[name]) {
-      // TODO: add assistant with name and config
-      OpenAIDriver.#assistants[name] = {to: 'do'}
-    }
-    return OpenAIDriver.#assistants[name]
+    return this.#config.driver
   }
 
   /**
    * Instructs the agent with the given name.
    * @param {Message} message
    */
-  // eslint-disable-next-line no-unused-vars
   async instruct(message) {
-    // TODO: send prompt to this.#thread and return response
-    // TODO: instead of a response maybe we should have a streaming response interface. Gotta see when we play with the API.
+    if (this.#run || !this.#available) {
+      this.#messageQueue.push(message)
+      this.#api.log.debug('OpenAI driver for agent', this.#agentName, 'is running, queueing message', message)
+      return
+    }
+    this.#run = new Run({
+      api: this.#api,
+      openai: this.#openai,
+      threadId: this.#thread.id,
+      assistantId: this.#assistant.id,
+      agentName: this.#agentName
+    })
+    this.#run.on('error', e => {
+      this.#api.log.error('OpenAI run error', e)
+      this.#run = null
+      if (this.#messageQueue.length) {
+        this.instruct(this.#messageQueue.shift())
+      }
+    })
+    this.#run.on('complete', messages => {
+      this.#api.log.debug('OpenAI run complete with messages', messages) //trace
+      messages.forEach(message => this.#api.comms.emit(message))
+      this.#run = null
+      if (this.#messageQueue.length) {
+        this.instruct(this.#messageQueue.shift())
+      }
+    })
+    this.#run.on('cancelled', () => {
+      this.#api.log.debug('OpenAI run cancelled')
+      this.#run = null
+    })
+
+    while (this.#messageQueue.length) {
+      await this.#run.addMessage(this.#messageQueue.shift())
+    }
+    await this.#run.addMessage(message)
+    this.#run.start()
   }
 
+  /**
+   * Pauses the driver. This will prevent further runs from being created.
+   */
   pause() {
+    this.#api.log.debug('Pausing OpenAI driver for agent', this.#agentName)
+    this.#available = false
   }
 
+  /**
+   * Resumes the driver. This will allow runs to be created again. If there are any queued messages, a run will be created for them.
+   */
   resume() {
+    this.#api.log.debug('Resuming OpenAI driver for agent', this.#agentName)
+    this.#available = true
+    if (this.#messageQueue.length) {
+      this.instruct(this.#messageQueue.shift())
+    }
+  }
+
+  /**
+   * Cleans up the driver by stopping the run and deleting the thread and assistant.
+   * @param {Object} assistant The assistant object from the OpenAI API
+   * @param {string} assistant.id The id of the assistant
+   */
+  async #cleanup(assistant) {
+    if (this.#run) {
+      try {
+        this.#run.stop()
+        this.#api.log.debug('Stopped OpenAI run', this.#run.id)
+      } catch (e) {
+        this.#api.log.error('Failed to stop OpenAI run', this.#run.id, e)
+      }
+    }
+
+    if (!this.#config.driver.keepThread) {
+      try {
+        this.#openai.beta.threads.del(this.#thread.id)
+        this.#api.log.debug('Deleted OpenAI thread', this.#thread.id)
+      } catch (e) {
+        this.#api.log.error('Failed to delete OpenAI thread', this.#thread.id, e)
+      }
+    }
+
+    if (!this.#config.driver.keepAssistant) {
+      try {
+        this.#openai.beta.assistants.del(assistant.id)
+        this.#api.log.debug('Deleted OpenAI assistant', assistant.id)
+      } catch (e) {
+        this.#api.log.error('Failed to delete OpenAI assistant', assistant.id, e)
+      }
+    }
   }
 }
---
+++ run.js
@@ -0,0 +1,215 @@
+import EventEmitter from 'events'
+
+/**
+ * A run is a single conversation with an assistant.
+ * @fires Run#error - when an error occurs
+ * @fires Run#complete - when the run is complete
+ * @fires Run#cancelled - when the run is cancelled
+ */
+export default class Run extends EventEmitter {
+  #api
+  #openai
+  #threadId
+  #assistantId
+  #run
+  #interval
+  #agentName
+  #status = 'queued'
+
+  /**
+   * Creates a new run
+   * @param {API} api The API instance
+   * @param {Object} openai The OpenAI instance
+   * @param {string} threadId The thread id on which to run
+   * @param {string} assistantId The assistant id to use for the run
+   * @param {string} agentName The agent name for debugging and messaging
+   * @emits Run#error
+   * @emits Run#complete
+   * @emits Run#cancelled
+   * @throws {Error} if the run cannot be created, started, or any other error occurs with the API.
+   */
+  constructor({api, openai, threadId, assistantId, agentName}) {
+    super()
+    this.#api = api
+    this.#openai = openai
+    this.#threadId = threadId
+    this.#assistantId = assistantId
+    this.#agentName = agentName
+  }
+
+  /**
+   * The status of the run
+   * @type {string}
+   */
+  get status() {
+    return this.#status
+  }
+
+  /**
+   * Adds a message to a run
+   * @param {Message} message
+   */
+  async addMessage(message) {
+    const oaiMessage = await this.#openai.beta.threads.messages.create(this.#threadId, {
+      role: 'user',
+      content: message.content,
+      metadata: {
+        source: message.source,
+        target: message.target,
+        id: message.id,
+        timestamp: message.timestamp.getTime(),
+        type: message.type.toString()
+      }
+    })
+    this.#api.log.debug('Created OpenAI message', oaiMessage, oaiMessage.content[0].text.value) //trace
+  }
+
+  /**
+   * Starts the run. Returns when the run is started.
+   */
+  async start() {
+    if (this.#interval) {
+      this.#api.log.warn('Run already started')
+      return
+    }
+
+    this.#run = await this.#openai.beta.threads.runs.create(this.#threadId, {
+      assistant_id: this.#assistantId
+    })
+    this.#api.log.debug('Created OpenAI run', this.#run)//trace
+
+    this.#interval = setInterval(() => this.#poll(), 5000)
+  }
+
+  /**
+   * Polls the server for the status of the run and handles the result
+   * @return {Promise<void>}
+   */
+  async #poll() {
+    try {
+      const runResult = await this.#openai.beta.threads.runs.retrieve(this.#threadId, this.#run.id)
+      this.#status = runResult.status
+      this.#api.log.debug(`OpenAI run ${this.#run.id} for agent ${this.#agentName} is ${runResult.status}`)//trace
+      switch (runResult.status) {
+        //case 'queued':
+        //case 'in_progress':
+        //case 'cancelling':
+      case 'requires_action':
+        this.#onActionRequired(runResult)
+        break
+      case 'cancelled':
+        this.#onCancel(runResult)
+        break
+      case 'expired':
+      case 'failed':
+        this.#onError(runResult)
+        break
+      case 'completed':
+        await this.#onComplete(runResult)
+        break
+      }
+    } catch (e) {
+      this.onError(e)
+    }
+  }
+
+  /**
+   * Error event.
+   * @event Run#error
+   * @type {object}
+   * @property {string} message - The error message.
+   */
+
+  /**
+   * Handles an error from the run
+   * @param {Object|Error} response
+   * @param {Object} [response.last_error]
+   * @param {string} [response.last_error.message]
+   * @fires Run#error
+   */
+  #onError(response) {
+    clearInterval(this.#interval)
+    if (response instanceof Error) {
+      this.emit('error', response.message)
+    } else if (!response.last_error) {
+      this.emit('error', response.last_error.message)
+    } else {
+      this.emit('error', response)
+    }
+  }
+
+  /**
+   * Complete event.
+   * @event Run#complete
+   * @type {object}
+   * @property {Array} messages - The messages from the run.
+   */
+
+  /**
+   * Handles the completion of a run
+   */
+  async #onComplete() {
+    clearInterval(this.#interval)
+    const messages = await this.#getMessagesFromLastRun()
+    this.#api.log.debug('Run result:', messages)//trace
+    this.emit('complete', messages)
+  }
+
+  /**
+   * Gets the messages from the last run.
+   * @return {Promise<*[]>}
+   */
+  async #getMessagesFromLastRun() {
+    const messages = []
+    const oaiMessages = await this.#openai.beta.threads.messages.list(this.#threadId, {
+      limit: 5, // get the last few messages in case the AI generated more than one
+      order: 'desc'
+    })
+
+    const runCreateAt = this.#run.created_at
+    for (const oaiMessage of oaiMessages.data) {
+      if (oaiMessage.role === 'user') {
+        continue
+      }
+
+      if (oaiMessage.created_at < runCreateAt) {
+        this.#api.log.debug('Skipping sent message', oaiMessage) //trace
+        continue
+      }
+
+      for (const content of oaiMessage.content) {
+        // TODO parse annotation from openAI text message and convert to html
+        messages.push(this.#api.comms.createMessage('user', this.#agentName, content.text.value))
+      }
+    }
+    return messages
+  }
+
+  /**
+   * Stops the run. Returns when the run is stopped. Indirectly fires the cancel event when the request returns.
+   */
+  async stop() {
+    await this.#openai.beta.threads.runs.cancel(this.#threadId, this.#run.id)
+  }
+
+  /**
+   * Cancel event.
+   * @event Run#cancelled
+   * @type {object}
+   */
+
+  /**
+   * Handles the cancellation of a run.
+   * @fires Run#cancelled
+   */
+  #onCancel() {
+    clearInterval(this.#interval)
+    this.#api.log.debug('Cancelled OpenAI run', this.#run.id)
+    this.emit('cancelled')
+  }
+
+  #onActionRequired(runResult) {
+    // TODO handle functions and skills
+    this.#api.log.debug('OpenAI run requires action', runResult)
+  }
+}
